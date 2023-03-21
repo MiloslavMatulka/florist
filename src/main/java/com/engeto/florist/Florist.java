0b69f2cb37e3e305d7a19c11d75a7a20d8ccfa11
Miloslav Matulka <miloslav.matulka@gmail.com>
2023-03-21 14:33:55 +0100

    Extract methods
    Make the main method more readable.

--- src/main/java/com/engeto/florist/Florist.java
+++ src/main/java/com/engeto/florist/Florist.java
@@ -9,6 +9,51 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Florist {
+    private static void deleteNonpoisonousItems(Flower[] flowers,
+                                                Statement statement,
+                                                String table)
+            throws SQLException {
+        statement.executeUpdate("DELETE FROM " + table
+                + " WHERE is_poisonous = false");
+        for (int i = 0; i < flowers.length; ++i) {
+            if (!flowers[i].isPoisonous()) {
+                flowers[i] = null;
+            }
+        }
+    }
+
+    private static void insertItems(Flower[] flowers, Statement statement,
+                                    String table) throws SQLException {
+        for (Flower flower : flowers) {
+            statement.executeUpdate("INSERT INTO " + table
+                    + " VALUES " + "(" + flower.getId() + ", '"
+                    + flower.getFlowerName() + "', '"
+                    + flower.getColor() + "', '"
+                    + flower.getDescription() + "', "
+                    + flower.isPoisonous()
+                    + ")");
+        }
+    }
+
+    private static void printNames(Statement statement, String table)
+            throws SQLException {
+        statement.executeQuery("SELECT * FROM " + table);
+        ResultSet result = statement.getResultSet();
+        while (result.next()) {
+            System.out.println(result.getString("flower_name"));
+        }
+    }
+
+    private static void updateDescription(Flower flower, Statement statement,
+                                          String table, String description)
+            throws SQLException {
+        statement.executeUpdate("UPDATE " + table + " SET "
+                + "description = '" + description
+                + "' WHERE flower_name = '"
+                + flower.getFlowerName() + "'");
+        flower.setDescription(description);
+    }
+
     public static void main(String[] args) {
         Logger logger = Logger.getLogger(Florist.class.getName());
 
@@ -23,39 +68,22 @@ public class Florist {
                 "mysql.florist",
                 "flor54iSt")) {
             Statement statement = connection.createStatement();
+
             String table = "florist.flower";
+            // Clear the table to enable running the program repetitively
             statement.executeUpdate("DELETE FROM " + table);
-            for (Flower flower : flowers) {
-                statement.executeUpdate("INSERT INTO " + table
-                        + " VALUES " + "(" + flower.getId() + ", '"
-                        + flower.getFlowerName() + "', '"
-                        + flower.getColor() + "', '"
-                        + flower.getDescription() + "', "
-                        + flower.isPoisonous()
-                        + ")");
-            }
+
+            insertItems(flowers, statement, table);
 
             String snowflakeDescriptionUpdate =
                     "Pozor na cibulku - obsahuje největší koncentraci jedu!";
-            statement.executeUpdate("UPDATE " + table + " SET "
-                    + "description = '" + snowflakeDescriptionUpdate
-                    + "' WHERE flower_name = '"
-                    + snowflake.getFlowerName() + "'");
-            snowflake.setDescription(snowflakeDescriptionUpdate);
+            updateDescription(snowflake, statement, table,
+                    snowflakeDescriptionUpdate);
 
-            statement.executeUpdate("DELETE FROM " + table
-                    + " WHERE is_poisonous = false");
-            for (int i = 0; i < flowers.length; ++i) {
-                if (!flowers[i].isPoisonous()) {
-                    flowers[i] = null;
-                }
-            }
+            deleteNonpoisonousItems(flowers, statement, table);
 
-            statement.executeQuery("SELECT * FROM " + table);
-            ResultSet result = statement.getResultSet();
-            while (result.next()) {
-                System.out.println(result.getString("flower_name"));
-            }
+            System.out.println("All flowers in table:");
+            printNames(statement, table);
         } catch (SQLException e) {
             logger.log(Level.WARNING, "Chyba při komunikaci s databází: "
                     + e.getLocalizedMessage());
